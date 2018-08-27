99d911ab9dfd93da88eb6f12bd4a6c81a36bc10f
Spencer Brooks <spencer@databit.io>
2018-08-26 22:01:18 -0600

    ready to ship; fades are timed correctly

--- main.js
+++ main.js
@@ -4,9 +4,13 @@ $(document).ready(function() {
     turned_over: 0,
     color_classes: ['clr-1', 'clr-1', 'clr-2', 'clr-2', 'clr-3', 'clr-3', 'clr-4', 'clr-4', 'clr-5', 'clr-5', 'clr-6', 'clr-6'],
     init: function() {
-      app.colorCards()
-      // $('.main-container').animate({opacity: 1}, 2000)
-      app.shuffle()
+      app.splash()
+
+      setTimeout(function() {
+        app.colorCards()
+        app.shuffle()
+      }, 2000)
+
     },
     shuffle: function() {
       var random = 0
@@ -33,22 +37,19 @@ $(document).ready(function() {
       
       $('.card').on('click', function() {
         console.log(app.turned_over)
-        if (app.turned_over === 0 || app.turned_over === 1) {
+        if ( $(this).hasClass('selected') === false && (app.turned_over === 0 || app.turned_over === 1) ) {
           $(this).html('<div><p>' + $(this).data('cardValue') + '</p></div>' ).addClass('selected')
+          $('.card p').animate({opacity: 1}, 500)
           app.turned_over += 1
           app.checkMatch()
         }
       })
+      // app.turned_over === 0 || app.turned_over === 1
 
     },
     checkMatch: function() {
       if ($('.selected').length == 2) {
         if ($('.selected').first().data('cardValue') === $('.selected').last().data('cardValue')) {
-          // cards matched
-          // remove cards
-          
-
-
 
           $('.selected').each(function() {
             $(this).animate({opacity: 0})
@@ -58,25 +59,27 @@ $(document).ready(function() {
               $(this).html('').removeClass('unmatched').removeClass('selected').addClass('no-hover')
             })
             app.turned_over = 0
+            app.checkWin()
           }, 1000)
-          app.checkWin()
+          // app.checkWin()
           
         } else {
-          // cards didn't match
-          // flip cards back over
+
+          $('.card p').animate({opacity: 0}, 1000)
           setTimeout(function() {
             app.turned_over = 0
             $('.selected').each(function() {
               $(this).html(' ').removeClass('selected')
               })
-            }, 1000)
+            }, 1500)
 
         }
       }
     },
     checkWin: function() {
-      if ( $('.unmatched').length === 0 ) {
+      if ( $('.unmatched').length === 0) {
         $('.container').eq(1).html('<p>You Win</p>').addClass('won-class')
+        $('.container').eq(1).animate({opacity: 1}, 3000)
       }
     },
     colorCards: function() {
@@ -89,6 +92,14 @@ $(document).ready(function() {
         $(element).animate({opacity: 1}, (Math.pow(Math.random(), 2) * 3000))
         app.color_classes.splice(rand_color_index, 1)
       })
+    },
+    splash: function() {
+      var container_clone = $('.container').eq(1).clone()
+      $('.container').eq(1).html('<p>Welcome</p>').addClass('splash-class')
+      $('.splash-class').animate({opacity: 0}, 2000)
+      setTimeout(function() {
+        $('.container').eq(1).replaceWith(container_clone)
+      }, 2000)
     }
   }
   app.init()
